40de8892c88630a42650bd94c2d2ff1aabd065c2
Ste74 <stefano@manjaro.org>
2018-05-20 22:33:23 +0200

    Corrected script
    Signed-off-by: Ste74 <stefano@manjaro.org>

--- backgrounds-xml.sh
+++ backgrounds-xml.sh
@@ -19,8 +19,8 @@ echo "OK. Now we are creating $WALL.xml"
 
 # This script is looking for .png and .jpg files only, but you can add here another file types. The "<options>stretched</options>" should work best for unknow sized files.
 for i in $(ls *jpg); do #Note if we add png files simply add $(ls *png)
-echo "<wallpaper deleted="false">
-<name>$i</name>
+echo "<wallpaper deleted="\"false\"">
+<name>${i::(-4)}</name>
 <filename>${ABSPATH}$i</filename>
   <options>stretched</options>
     <pcolor>#ffffff</pcolor>
